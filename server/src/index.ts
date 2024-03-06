098ee71233b6a389a825e279ac5afce58a7fefd1
Paul Felkner <paul.felkner@elunic.com>
2024-03-06 18:07:03 +0100

    server: working version of planetscale/prisma integration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,67 +1,33 @@
-// import express from "express";
-// import * as trpcExpress from "@trpc/server/adapters/express";
-// import { appRouter } from "./routers/_app";
-
-// const app = express();
-
-// app.use(
-//   "/trpc",
-//   trpcExpress.createExpressMiddleware({
-//     router: appRouter,
-//     createContext: () => ({}), // Create context for each request
-//   }),
-// );
-
-// app.listen(4000);
-// console.log("Server listening on http://localhost:4000");
-
-// import { initTRPC } from "@trpc/server";
-import * as trpcExpress from "@trpc/server/adapters/express";
 import express from "express";
-// import { appRouter } from "./routers/_app";
 import { Prisma, PrismaClient } from "@prisma/client";
-import * as cors from "cors";
+import cors = require("cors");
 const prisma = new PrismaClient();
 
-// // created for each request
-// const createContext = ({
-//   req,
-//   res,
-// }: trpcExpress.CreateExpressContextOptions) => ({}); // no context
-// type Context = Awaited<ReturnType<typeof createContext>>;
-
-// const t = initTRPC.context<Context>().create();
-// const router = appRouter;
-
 const app = express();
-app.use(cors.default);
-app.listen(4000, () => {
-  console.log(`[server]: Server is running at http://localhost:${4000}`);
+app.use(cors());
+const port = 8080;
+app.use(express.json());
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-// app.use(express.json());
 
 app.get("/", async (req, res) => {
+  console.log("get request at /");
   const users = await prisma.user.findMany();
   console.log("users:", users);
   res.json(users);
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
+  const name = req.body.name;
+  if (!name) {
+    res.status(400).send("Name is required");
+    return;
+  }
   const user = await prisma.user.create({
     data: {
-      name: "Alice",
+      name: name,
     },
   });
-  // const users = await prisma.user.findMany();
-  // console.log("users:", users);
-  res.json(user);
+  res.send(user);
 });
-
-// app.use(
-//   "/trpc",
-//   trpcExpress.createExpressMiddleware({
-//     router: appRouter,
-//     createContext,
-//   })
-// );
