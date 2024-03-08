308cde21d791551d24b12d73abe1a4016120a83e
Paul Felkner <paul.felkner@elunic.com>
2024-03-08 13:08:39 +0100

    avoid duplicate flags

--- client/src/hooks/getCountry.js
+++ client/src/hooks/getCountry.js
@@ -1,17 +1,27 @@
 import { useState, useEffect } from "react";
 import data from "./../countries.json";
+let previousCountries = [];
+
+const getRandom = (ceil) => {
+  return Math.floor(Math.random() * ceil);
+};
 
 function getCountry() {
+  console.log("Get Country");
   const countries = data.countries;
   const keys = Object.keys(countries);
   const values = Object.values(countries);
 
   const getRandomEntries = () => {
     let randomEntries = [];
-    for (let i = 0; i < 4; i++) {
-      const randomIndex = Math.floor(Math.random() * keys.length);
-      randomEntries.push(randomIndex);
+    for (let i = 0; randomEntries.length < 4; i++) {
+      const randomIndex = getRandom(keys.length);
+      if (!previousCountries.includes(randomIndex)) {
+        randomEntries.push(randomIndex);
+        previousCountries.push(randomIndex);
+      }
     }
+    console.log(previousCountries);
     return randomEntries;
   };
 
