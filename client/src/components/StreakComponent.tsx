b1b54b028b8c133dc06550cd8f7d3976970c36e5
Paul Felkner <paul.felkner@elunic.com>
2024-03-11 00:21:05 +0100

    update score /streak) on leave

--- client/src/components/StreakComponent.tsx
+++ client/src/components/StreakComponent.tsx
@@ -3,14 +3,36 @@ import React, { useEffect, useState } from "react";
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import AutoGraphOutlinedIcon from "@mui/icons-material/AutoGraphOutlined";
 import { green, red, blue, yellow } from "@mui/material/colors";
+import axios from "axios";
+import useUser from "../context/_UserContext";
 
 interface StreakComponentProps {
   streakCount: number;
 }
 
+
+
 const StreakComponent = ({ streakCount }:StreakComponentProps ) => {
   // const [streakCount, setStreakcount] = useState(0);
   const [highestStreak, setHighestStreak] = useState(0);
+  const userContext = useUser();
+
+  useEffect(() => {
+    // Empty function for initial effect
+    return () => {
+      // This function will be called when the component unmounts
+      axios.post('http://localhost:8080/score/update', {
+        userId: userContext!.user.id,
+        highestStreak: highestStreak,
+      })
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+        });
+    };
+  }, []);
 
   useEffect(() => {
     if (streakCount > highestStreak) {
